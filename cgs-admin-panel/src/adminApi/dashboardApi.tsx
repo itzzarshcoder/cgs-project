d68bb559d78b5f73e7ce6ffa32c73b4911ee2759
itzzarshcoder <itzzarsh001@gmail.com>
2026-03-17 11:29:11 +0530

    Delete cgs-admin-panel directory

--- cgs-admin-panel/src/adminApi/dashboardApi.tsx
+++
@@ -1,6 +0,0 @@
-import adminInstance from "./adminInstance";
-
-export const getDashboardData = async () => {
-  const response = await adminInstance.get("/dashboard/get");
-  return response.data;
-};
